7ccd5161d1eed08c76b1f7a41380bdb186958571
Loaye <jr.carstens00@gmail.com>
2017-11-28 14:53:41 -0800

    creates registration views and handeling

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -128,3 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
