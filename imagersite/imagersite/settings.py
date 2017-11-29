e4cdbc10507eb2a6d551fe152d856232be21ce05
Philip Werner <philip.r.werner@gmail.com>
2017-11-29 14:37:05 -0800

    settings changes

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'imagersite',
     'imager_profile',
     'imager_images'
 ]
@@ -127,3 +128,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
